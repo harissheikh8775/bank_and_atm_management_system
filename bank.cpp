71014413f355cd2905562a06ff9a02361cd93294
harissheikh8775 <harisbadboi@gmail.com>
2024-09-06 12:10:14 +0530

    bank and atm management fucntions created

--- bank.cpp
+++ bank.cpp
@@ -32,6 +32,7 @@ p:
     switch (choice)
     {
     case 1:
+        system("cls");
         cout << "\n\n\t\t\tLogin Account";
         cout << "\n\n E-mail : ";
         cin >> email;
@@ -77,11 +78,87 @@ p:
 // function for bank_management
 void bank::bank_management()
 {
+p:
+    system("cls");
+    int choice;
+    cout << "\n\n\t\t\tBank Management System";
+    cout << "\n\n 1. User Login & Check Balance";
+    cout << "\n 2. Withdraw Amount";
+    cout << "\n 3. Account Details";
+    cout << "\n 4. Go Back";
+    cout << "\n\n Enter Your Choice : ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        menu();
+    default:
+        cout << "\n\n Invalic Value...Please Try Again....";
+        break;
+    }
+    getch(); // like mainloop
+    goto p;  // repeating the function
 }
 
 // function for atm_management
 void bank::atm_management()
 {
+p:
+    system("cls");
+    int choice;
+    cout << "\n\n\t\t\tATM Management System";
+    cout << "\n\n 1. New User";
+    cout << "\n 2. Already User";
+    cout << "\n 3. Deposit Option";
+    cout << "\n 4. Withdraw Option";
+    cout << "\n 5. Transfer Option";
+    cout << "\n 6. Payment Option";
+    cout << "\n 7. Search User Record";
+    cout << "\n 8. Edit User Record";
+    cout << "\n 9. Delete User Record";
+    cout << "\n 10. Show All Records ";
+    cout << "\n 11. Payment All Records ";
+    cout << "\n 12. Go Back ";
+    cout << "\n\n Enter Your Choice : ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    case 8:
+        break;
+    case 9:
+        break;
+    case 10:
+        break;
+    case 11:
+        break;
+    case 12:
+        menu(); // go back
+    default:
+        cout << "\n\n Invalic Value...Please Try Again....";
+        break;
+    }
+    getch(); // like mainloop
+    goto p;  // repeating the function
 }
 
 // main function
